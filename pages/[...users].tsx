84814cfa4554e04f905e3622cfe46ceb81a903d6
Mackenzie <guest173800@gmail.com>
2021-07-04 13:37:04 -0400

    any chatters?

--- pages/[...users].tsx
+++ pages/[...users].tsx
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
 const generateStreamUrl = (name: string): string => {
     return `https://player.twitch.tv/?channel=${name}&parent=localhost`
 }
 
-// eslint-disable-next-line no-unused-vars
 const generateChatUrl = (name: string): string => {
-    return `https://www.twitch.tv/embed/${name}/chat?parent=localhost`
+    return `https://www.twitch.tv/embed/${name}/chat?darkpopout&parent=localhost`
 }
 
-// change to getInitalProps for loading users
 const Streams = () => {
+    const [showChat, setShowChat] = useState(false);
+
     const router = useRouter();
     let { users } = router.query;
+
     if (!users) {
         return <p>Woah! How did you get here?</p>;
     }
     if (!Array.isArray(users)) {
         users = [users];
     }
+
     return (
-        <div className="flex h-screen w-screen">{
-            users.map((u) =>
-                <div className="w-3/6" key={u}>
-                    <iframe src={generateStreamUrl(u)} frameBorder="0" allowFullScreen={true} scrolling="no" className="w-full h-full" />
-                </div>
-            )
-        }
+        <div className="">
+            <button className="absolute text-white" onClick={() => setShowChat(!showChat)}>Toggle Chat</button>
+            <div className="grid grid-cols-2 grid-flow-row h-screen w-screen">
+                {
+                    users.map((u) =>
+                        <div className="flex flex-col" key={u}>
+                            <iframe
+                                src={generateStreamUrl(u)}
+                                frameBorder="0"
+                                allowFullScreen={true}
+                                scrolling="no"
+                                className={`w-full ${showChat ? 'h-1/2' : 'h-full'}`}
+                            />
+                            {
+                                showChat ?
+                                    <iframe
+                                        id="chat_embed"
+                                        src={generateChatUrl(u)}
+                                        className="h-1/2"
+                                    /> :
+                                    null
+                            }
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
